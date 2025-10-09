2b74a10562b298a9fa59ded0d8bf3938175768e0
AnamulHaqueN <rafaielanam@gmail.com>
2025-10-09 19:31:17 +0600

    Dashboard Route for guest user and login user is implemented

--- src/routes/_protected/dashboard.tsx
+++ src/routes/_protected/dashboard.tsx
@@ -8,5 +8,23 @@ export const Route = createFileRoute('/_protected/dashboard')({
 function RouteComponent() {
   const {user} = useAuth();
   console.log(user);
-  return <div>Hello "/_protected/dashboard"!</div>
+  return (
+    <div className="p-10">
+      {user?
+      (<>
+      <h2 className='text-4xl font-bold mb-2'>Wellcome to {user?.username}</h2>
+      <p className="text-white-800">
+        Role: Your role is {user?.role}
+      </p>
+      </>):
+      (<>
+      <h2 className='text-4xl font-bold mb-2'>Wellcome to Guest User</h2>
+      <p className="text-white-800">
+        Role: No role assigned to you
+      </p>
+      </>)
+      }
+      
+    </div>
+  )
 }
